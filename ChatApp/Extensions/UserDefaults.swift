c659663f54e9f9aa0f932f71a98d35df3a812fb1
Jason Jon E. Carreos <jason@ingenuity.ph>
2021-12-03 15:13:57 +0800

    Setup ContentManager

---
+++ ChatApp/Extensions/UserDefaults.swift
@@ -0,0 +1,37 @@
+//
+//  UserDefaults.swift
+//  ChatApp
+//
+//  Created by Jason Jon E. Carreos on 12/3/21.
+//  Copyright © 2021 Slomins. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+
+    // TODO: Define keys here
+    enum Keys: String {
+        case test = "test"
+    }
+
+    /// Returns a codable object `T` with the given key.
+    /// - Returns: Optional `T` object.
+    func getCodable<T: Codable>(forKey key: Keys) -> T? {
+        guard let data = self.object(forKey: key.rawValue) as? Data else {
+            return nil
+        }
+
+        return try? JSONDecoder().decode(T.self, from: data)
+    }
+
+    /// Stores a codable object.
+    func setCodable<T: Codable>(_ object: T, forKey key: Keys) {
+        guard let data = try? JSONEncoder().encode(object) else {
+            return
+        }
+
+        self.set(data, forKey: key.rawValue)
+    }
+
+}
